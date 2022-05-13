1f14393474519db1579d171c4c1989396fd82500
Lucas Gomes <lucasgomesbt@gmail.com>
2022-05-12 21:03:11 -0300

    HTML completo

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import { BiPlay } from 'react-icons/bi';
+import { IoIosArrowForward } from "react-icons/io";
 
 export default function Home() {
     const [typeUser, setTypeUser] = useState<"man" | "woman" | string>("");
@@ -16,11 +17,11 @@ export default function Home() {
             <Head>
                 <title>Home</title>
             </Head>
-            <main className="w-full h-full bg-gradient-to-r from-profile_first to-profile_secondary">
-                <div className="absolute w-full h-full overflow-hidden">
+            <main className="w-full h-full bg-gradient-to-r from-profile_first to-profile_secondary overflow-hidden">
+                <section className="absolute w-full h-full overflow-hidden">
                     <Image src="/assets/friends.png" alt="Menu" width={1480} height={810} layout="responsive" />
                     <BiPlay size={40} />
-                </div>
+                </section>
                 <header className="flex items-center justify-between px-7 pt-2">
                     <div className="flex  gap-10 z-10">
                         <button>
@@ -46,7 +47,7 @@ export default function Home() {
                         )}
                     </button>
                 </header>
-                <div className="h-screen max-w-[80%] relative top-[17rem] md_screen:top-[7rem] left-[2.8rem]">
+                <section className="h-screen max-w-[80%] relative top-[17rem] md_screen:top-[7rem] left-[2.8rem]">
                     <Image src="/assets/logo_friends.png" alt="Menu" width={328} height={99} objectFit="cover" />
                     <p className="text-white mt-8">NOW PLAYING</p>
                     <p className="text-white mt-1">They are back in Central Perk. For one night only.</p>
@@ -58,7 +59,112 @@ export default function Home() {
                             MORE INFO
                         </button>
                     </div>
-                </div>
+                </section>
+                <section className="flex relative flex-col mt-36 left-[19rem] gap-6">
+                    <div className="flex gap-1 items-center">
+                        <h3 className="text-white text-xl font-bold">
+                            New and Trending
+                        </h3>
+                        <IoIosArrowForward size={20} color="white" />
+                    </div>
+                    <div className="flex gap-3 overflow-scroll">
+                        <Image src="/assets/tom_jerry.png" alt="Tom e Jerry" width={548} height={307} objectFit="cover" />
+                        <Image src="/assets/gossip.png" alt="Gossip Girl" width={548} height={307} objectFit="cover" />
+                        <Image src="/assets/raised.png" alt="Raised by Wolves" width={548} height={307} objectFit="cover" />
+                    </div>
+                </section>
+                <section className="flex flex-col">
+                    <div className="flex gap-1 items-center">
+                        <h3 className="text-white text-xl font-bold">
+                            Movies We Love
+                        </h3>
+                        <IoIosArrowForward size={20} color="white" />
+                    </div>
+                    <div className="flex gap-3">
+                        <Image src="/assets/conjuring.png" alt="The Conjuring" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/sudden.png" alt="No Sudden Move" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/tom_jerry_capa.png" alt="Tom & Jerry" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/secret_garden_capa.png" alt="The Secret Garden" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/let_them.png" alt="Let Them All Talk" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/justice_league.png" alt="Justice League" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/guns.png" alt="Guns" width={200} height={300} objectFit="cover" />
+                    </div>
+                </section>
+                <section className="flex">
+                    <div className="flex flex-col gap-3">
+                        <h3 className="text-white font-bold">
+                            Essential Series
+                        </h3>
+                        <p className="text-white">
+                            Your favorite shows, beloved characters, and
+                            unforgettable stories.
+                        </p>
+                        <button>
+                            BROWSE ALL
+                        </button>
+                    </div>
+                    <div className="flex gap-3">
+                        <Image src="/assets/got.png" alt="Game of Thrones" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/big_bang.png" alt="The Big Bang Theory" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/sex_and_city.png" alt="Sex and The City" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/love_life.png" alt="Love Life" width={200} height={300} objectFit="cover" />
+                    </div>
+                </section>
+                <section className="flex relative flex-col">
+                    <div className="flex gap-1 items-center">
+                        <h3 className="text-white text-xl font-bold">
+                            Star Power
+                        </h3>
+                        <IoIosArrowForward size={20} color="white" />
+                    </div>
+                    <div className="flex gap-3">
+                        <Image src="/assets/will.png" alt="Will Smith" width={281} height={422} objectFit="cover" />
+                        <Image src="/assets/jennifer.png" alt="Jennifer Aniston" width={281} height={422} objectFit="cover" />
+                        <Image src="/assets/margot.png" alt="Margot Robbie" width={281} height={422} objectFit="cover" />
+                        <Image src="/assets/christopher.png" alt="Christopher Nolan" width={281} height={422} objectFit="cover" />
+                        <Image src="/assets/tom_hanks.png" alt="Tom Hanks" width={281} height={422} objectFit="cover" />
+                    </div>
+                </section>
+                <section className="flex flex-col">
+                    <div className="flex gap-1 items-center">
+                        <h3 className="text-white text-xl font-bold">
+                            Harry Potter and Other Fantastic Universes
+                        </h3>
+                        <IoIosArrowForward size={20} color="white" />
+                    </div>
+                    <div className="flex gap-3">
+                        <Image src="/assets/harry.png" alt="Harry Potter" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/fantastic.png" alt="Fantastic Beasts" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/lord.png" alt="The Lord Of The Rings" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/hunger_games.png" alt="The Hunger Games" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/catching_fire_2.png" alt="The Hunger Games Catching Fire 2" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/mockingjay_3.png" alt="The Hunger Games Mockingjay Part I" width={200} height={300} objectFit="cover" />
+                        <Image src="/assets/mockingjay_4.png" alt="The Hunger Games Mockingjay Part II" width={200} height={300} objectFit="cover" />
+                    </div>
+                </section>
+                <section className="flex items-center justify-center">
+                    <div className="flex flex-col">
+                        <div className="flex gap-3">
+                            <Image src="/assets/max_originals.png" alt="Max Originals" layout="fixed" width={60} height={26} />
+                            <p className="text-white">
+                                GOSSIP GIRL: THE BAD WITCH
+                            </p>
+                        </div>
+                        <div className="flex flex-col items-start justify-center">
+                            <h2>
+                                Hello again, Upper East Siders.
+                            </h2>
+                            <p>
+                                Your one and only source into the scandalous lives of Manhattan's
+                                elite is back, with a new cast.
+                            </p>
+                            <button>
+                                WATCH NOW
+                            </button>
+                        </div>
+                    </div>
+                    <Image src="/assets/footer.png" alt="Game of Thrones" width={360} height={360} objectFit="cover" />
+                </section>
             </main>
         </>);
 }
